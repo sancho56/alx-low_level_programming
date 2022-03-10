d6b34e8ea4c200d28ba8ae9774f45c0fa3224dae
sancho56 <samimeskele@outlook.com>
2022-03-10 07:48:03 -0800

    first c programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 
-/* This is a program that print a value */
+/*
+ * This is a program that 
+ * print a value
+ */
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
+
