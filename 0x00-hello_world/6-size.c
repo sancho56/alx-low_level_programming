55de55b3d56888481b8c9bdc6619ef2403345a5c
sancho56 <samimeskele@outlook.com>
2022-03-10 08:47:06 -0800

    Printing the size of various types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,11 +12,11 @@ int main(void)
 	long long int d;
 	float e;
 
-	printf("Size of char: %d byte(s)", a);
-	printf("Size of an int: %d byte(s)", b);
-	printf("Size of a long int: %d byte(s)", c);
-	printf("Size of a long long int: %d byte(s)", d);
-	printf("Size of a float: %d byte(s)", e);
+	printf("Size of char: %d byte(s)\n", a);
+	printf("Size of an int: %d byte(s)\n", b);
+	printf("Size of a long int: %d byte(s)\n", c);
+	printf("Size of a long long int: %d byte(s)\n", d);
+	printf("Size of a float: %d byte(s)\n", e);
 	return (0);
 }
 
