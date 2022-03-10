5ee07a95b72fbb8fe5ff4c596ee105c2d2202db6
sancho56 <samimeskele@outlook.com>
2022-03-10 08:41:27 -0800

    Printing the size of various types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -17,8 +17,6 @@ int main(void)
 	printf("Size of a long int: %d byte(s)", c);
 	printf("Size of a long long int: %d byte(s)", d);
 	printf("Size of a float: %d byte(s)", e);
-	
 	return (0);
-
 }
 
