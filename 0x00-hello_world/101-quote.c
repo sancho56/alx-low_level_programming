036ba0cbbb89088c42363cc1f15e824bb2d529ce
sancho56 <samimeskele@outlook.com>
2022-03-10 16:06:06 -0800

    adding quote in c programming

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-	echo "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	putchar( "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
 	return (1);
 }
 
