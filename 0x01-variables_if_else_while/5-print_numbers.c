2067798a671e3577620ca1d1d5d00cdb9c8e3f88
sancho56 <samimeskele@outlook.com>
2022-03-12 14:00:30 -0800

    single digit number

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - prints all single digit number
+ * Description: This program prints all 10 base single digit numbers
+ * Return: retunr 0
+ */
+int main(void)
+{
+	for (int i = 0; i < 10; i++)
+		printf(i);
+	return (0);
+}
+
