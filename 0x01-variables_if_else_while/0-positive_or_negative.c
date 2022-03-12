b2ba015702ca8c0fa4d1c42d303e925d6c4b5fe7
sancho56 <samimeskele@outlook.com>
2022-03-12 11:44:34 -0800

    checks the number for the positive or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,15 +14,15 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if(n>0)
 	{
-		printf("%d is postive",n);
+		printf("%d is postive\n",n);
 	}
 	else if (n==0)
 	{
-		printf("%d is zero",n);
+		printf("%d is zero\n",n);
 	}
 	else
 	{
-		printf("%d is negative",n);
+		printf("%d is negative\n",n);
 	}
 	return (0);
 }
