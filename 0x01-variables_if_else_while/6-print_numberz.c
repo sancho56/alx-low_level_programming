912f4cb232613b5d0fac96d0455642636380129b
sancho56 <samimeskele@outlook.com>
2022-03-12 14:24:19 -0800

    single digit number including zero

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - display single digit numbers
+ * Description: This program prints the single digit number from zero
+ * Return: return 0
+ */
+int main(void)
+{
+	int i;
+	for (i = 0; i < 10; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
+
