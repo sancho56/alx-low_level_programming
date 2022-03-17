cf6ef95f1aa9464d7d1a258ace97b068c032f6fd
sancho56 <samimeskele@outlook.com>
2022-03-17 13:46:19 -0700

    print alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -13,10 +13,12 @@ int main(void)
 }
 void print_alphabet(void)
 {
-	for (char i = 'a'; i <= 'z'; i++)
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
 	{
-		_putchar(i);
+		putchar(i);
 	}
-	_putchar('\n');
+	putchar('\n');
 }
 
