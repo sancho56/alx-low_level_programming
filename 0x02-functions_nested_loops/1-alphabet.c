11ba30aecb4d6c121596f53dd56eb9cfebc8b11a
sancho56 <samimeskele@outlook.com>
2022-03-17 14:33:50 -0700

    print alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,9 +2,8 @@
 #include <stdio.h>
 /**
  * print_alphabet() - print alphabet
- * @arg1: void nothing to pass
- * 
- * This funcition print alphabet in lowercase by looping through every point
+ * @arg: void nothing to pass
+ * Description: This funcition print alphabet in lowercase by looping through every point
  * Context: no sleeping
  * Return: void
  */
