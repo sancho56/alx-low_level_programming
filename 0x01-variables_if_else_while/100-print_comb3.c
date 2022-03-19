780afd5daa43183e7c18ee3287f9d5ed604a1317
sancho56 <samimeskele@outlook.com>
2022-03-19 06:20:24 -0700

    print all possible two digit combo

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - prints all possible two digit number
+ * Description:This program prints all possible combination of two digit
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = i + 1; j < 10; j++)
+		{
+			putchar(i + '0');
+			putchar(j + '0');
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+}
+
