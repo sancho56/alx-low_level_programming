77dc93d43d8bc8f9ae7e964736e818787ed7ecc4
sancho56 <samimeskele@outlook.com>
2022-03-19 22:59:01 -0700

    absoulte value of integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs - prints absoulte of integer
+ * @: integer
+ * Return: integer
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (-n);
+	}
+	else
+	{
+		return (n);
+	}
+}
