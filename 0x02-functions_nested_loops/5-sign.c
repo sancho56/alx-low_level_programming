59605c09c77c51537dd4a0d7ef4a971871108bbd
sancho56 <samimeskele@outlook.com>
2022-03-19 22:28:40 -0700

    print sign of number

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -8,16 +8,14 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		return ('+' + 1);
+		return (1);
 	}
 	else if (n == 0)
 	{
-		putchar('0');
 		return (0);
 	}
 	else
 	{
-		putchar('-');
 		return (-1);
 	}
 }
