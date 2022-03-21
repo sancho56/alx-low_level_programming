9fb0438fa36e349ce85f4404022ab20edfe078c3
sancho56 <samimeskele@outlook.com>
2022-03-20 21:01:40 -0700

    all natural numbers upto 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -17,8 +17,10 @@ void print_to_98(int n)
 				putchar((i / 10) + '0');
 				putchar((i % 10) + '0');
 				if (i != 98)
+				{
 					putchar(',');
-				putchar(' ');
+					putchar(' ');
+				}
 			}
 			else
 			{
