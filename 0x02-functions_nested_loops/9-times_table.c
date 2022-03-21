e829b02ccb5cf8fc76b250e44a8340169bd924f1
sancho56 <samimeskele@outlook.com>
2022-03-20 20:19:25 -0700

    multiplication table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -16,8 +16,10 @@ void times_table(void)
 			mul = i * j;
 			if (mul > 9)
 			{
-				if (mul != 0)
+				if (j != 0)
+				{
 					putchar(' ');
+				}
 				putchar((mul / 10) + '0');
 				putchar((mul % 10) + '0');
 				if (j != 9)
