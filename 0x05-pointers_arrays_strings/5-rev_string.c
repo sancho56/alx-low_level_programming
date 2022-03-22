c4b8062cc841047dd330cf3ed00147ca3d165785
sancho56 <samimeskele@outlook.com>
2022-03-22 16:57:56 -0700

    reverse string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * rev_string - reverse a string
+ * @s: character pointer
+ * Return: void
+ */
+void rev_string(char *s)
+{
+	char rev[10];
+	int i;
+	int a;
+	int c = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	}
+
+	for (a = i - 1; a >= 0; a--)
+	{
+		rev[c] = a;
+		c++;
+	}
+
+	s = rev;
+}
