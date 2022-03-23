57cabbafb705096c03c6decb2c51ce05085f56e6
sancho56 <samimeskele@outlook.com>
2022-03-22 17:17:23 -0700

    string jump print

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,16 @@
+#include "mian.h"
+/**
+ * puts2 - prints even string
+ * @str: char pointer
+ * Return: void
+ */
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i = i + 2)
+	{
+		putchar(str[i]);
+	}
+	putchar('\n');
+}
