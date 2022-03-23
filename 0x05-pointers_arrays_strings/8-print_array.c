bedb7219b5a7e7f4bc8b131ff5da6add7aa07c35
sancho56 <samimeskele@outlook.com>
2022-03-22 17:56:19 -0700

    print half string

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_array - prints the element of array
+ * @a: integer pointer
+ * @n: integer
+ */
+void print_array(int *a int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d, ", a[i]);
+	}
+}
