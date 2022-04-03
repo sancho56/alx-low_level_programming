3f1f02c0cf2c0738a738aaf09582f6d3bcc38bfd
sancho56 <samimeskele@outlook.com>
2022-04-02 20:59:16 -0700

    copy string

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -10,14 +10,14 @@ char *_strcpy(char *dest, char *src)
 	int i;
 	int n;
 
-	for (n = 1; src[n] != '\0'; n++)
+	for (n = 0; src[n] != '\0'; n++)
 	{
 	}
 
 	for (i = 0; i < n && src[i] != '\0'; i++)
 		dest[i] = src[i];
 
-	for ( ; i < n; i++)
+	for ( ; i <= n; i++)
 		dest[i] = '\0';
 
 	return (dest);
