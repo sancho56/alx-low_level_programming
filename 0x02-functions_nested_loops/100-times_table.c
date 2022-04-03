45d9a908525fc7e005ffbdd4af478a925299792d
sancho56 <samimeskele@outlook.com>
2022-04-03 13:27:37 -0700

    time table

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_times_table - prints the n times of number
+ * @n: integer
+ * Return: void
+ */
+void print_times_table(int n)
+{
+	int i;
+	int j;
+
+	for (i = 0; i <= n; i++)
+	{
+		for (j = 0; j <= n; j++)
+		{
+			printf("%d", i * j);
+			printf(", ");
+		}
+		printf("\n");
+	}
+}
