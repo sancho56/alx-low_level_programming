76d6d12572dcda5fcb697964ce9688d13c58584f
sancho56 <samimeskele@outlook.com>
2022-04-02 21:15:57 -0700

    convert string to int

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -7,13 +7,20 @@
 int _atoi(char *s)
 {
 	int i;
+	int exist = 0;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (i > '0' && i < '9')
 		{
 			putchar(i + '0');
+			exist = 1;
 		}
 	}
 	putchar('\n');
+
+	if (exist != 1)
+		return 0;
+	else
+		return 1;
 }
