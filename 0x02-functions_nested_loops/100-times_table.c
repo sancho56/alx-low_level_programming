210a8aac3fc59bc4a9a1b81e9af2898b7cf59b4c
sancho56 <samimeskele@outlook.com>
2022-04-03 13:43:01 -0700

    time table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -19,7 +19,7 @@ void print_times_table(int n)
 		for (j = 0; j <= n; j++)
 		{
 			product = i * j;
-			if (product == 0)
+			if (j == 0)
 				printf("%d", i * j);
 			else if (product < 9)
 				printf("  %d", i * j);
