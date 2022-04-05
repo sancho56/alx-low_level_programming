6795598083c7d63201a5534d960729a1597df6e7
sancho56 <samimeskele@outlook.com>
2022-04-05 15:24:34 -0700

    pointer of 2 dimen-array

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * alloc_grid - return pointer to 2 dimen-array
+ * @width: integer
+ * @height: integer
+ * Return: pointer of pointer integer
+ */
+int **alloc_grid(int width, int height)
+{
+	int **a;
+	int *wid = (int *) malloc(width);
+	int *hei = (int *) malloc(height);
+	int i, j;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	for (i = 0; i < wid; i++)
+	{
+		for (j = 0; j < hei; j++)
+		{
+			a[i][j] = 0;
+		}
+	}
+
+	if (a == NULL)
+		return (NULL);
+
+	return a;
+}
