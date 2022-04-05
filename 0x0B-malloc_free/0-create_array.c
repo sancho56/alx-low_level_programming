96cbfe55b0a4469767b3add971bb7f8315594106
sancho56 <samimeskele@outlook.com>
2022-04-05 04:38:47 -0700

    malloc first project

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,10 +10,13 @@ char *create_array(unsigned int size, char c)
 	char *p = (char *) malloc(size);
 	unsigned int i = 0;
 
-	while (i < size)
+	if (p != null)
 	{
-		p[i] = c;
-		i++;
+		while (i < size)
+		{
+			p[i] = c;
+			i++;
+		}
 	}
 	if (size == 0)
 		return (NULL);
