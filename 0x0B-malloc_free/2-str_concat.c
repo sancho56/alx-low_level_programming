4797531e455d12008f2fccc83f841632453c1fbc
sancho56 <samimeskele@outlook.com>
2022-04-08 23:15:47 -0700

    concatenate using malloc

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,13 +15,13 @@ char *str_concat(char *s1, char *s2)
 
 	if (s1 == NULL && s2 == NULL)
 	{
-		s1 = " ";
-		s2 = " ";
+		s1 = "";
+		s2 = "";
 	}
 	else if (s1 == NULL)
-		s1 = " ";
+		s1 = "";
 	else if (s2 == NULL)
-		s2 = " ";
+		s2 = "";
 
 
 	a = strlen(s1);
