9e68164824d9443b4549e45fd58d2037dad2de42
sancho56 <samimeskele@outlook.com>
2022-04-12 15:29:01 -0700

    prints name of complied file

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints the file name of complied
+ * Return: zero
+ */
+int main(int argc, char **argv)
+{
+	printf("%s", argv[0]);
+	return (0);
+}
