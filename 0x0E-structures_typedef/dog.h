a383af14c086c09df46673c3a0e7a70f9c3dc627
sancho56 <samimeskele@outlook.com>
2022-04-19 09:10:30 -0700

    struct project 2

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,8 @@
 #ifndef DOG_H
 #define DOG_H
 
+#include <stdio.h>
+
 /**
  * struct dog - structure type of dog
  * @name: char pointer
@@ -8,13 +10,13 @@
  * @owner: char pointer
  */
 
-void init_dog(struct dog *d, char *name, float age, char *owner);
-
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
 
 #endif
