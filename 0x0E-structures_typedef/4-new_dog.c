855594c55112d1c8f8b2a93714d2c5524da9fdf9
sancho56 <samimeskele@outlook.com>
2022-04-20 13:10:57 -0700

    prints new dog

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+/**
+ * new_dog - function that creats new dog
+ * @name: char pointer
+ * @age: float
+ * @owner: char pointer
+ * Return: dog_t
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *new_obj;
+	dog_t->name = name;
+	dog_t->age = age;
+	dog_t->owner = owner;
+
+	return new_obj;
+}
