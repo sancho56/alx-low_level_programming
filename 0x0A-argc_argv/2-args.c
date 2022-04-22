f18b7ac15064b34bd5d91f8f966e38f5497c06bb
sancho56 <samimeskele@outlook.com>
2022-04-22 00:29:25 -0700

    prints all command arguments

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <<stdio.h>
+/**
+ * main - prints all arguments it receives
+ * @argc: integer
+ * @argv: char pointer
+ * Return: zero
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+
+	return (0);
+}
