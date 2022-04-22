12800f3090d8fa2421285233e0bb3ba1e5ed457c
sancho56 <samimeskele@outlook.com>
2022-04-22 00:02:20 -0700

    prints count of command arguments

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,4 +1,3 @@
-#include "main.h"
 #include <stdio.h>
 /**
  * main - prints the number of arguments passed
@@ -8,7 +7,7 @@
  */
 int main(int argc, char *argv[])
 {
-	if (argc != 0)
+	if (argc != 0 && argv)
 		printf("%d\n", (argc - 1));
 	return (0);
 }
