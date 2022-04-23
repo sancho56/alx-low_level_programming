64cc47fee0e536409aa3b6ec14eaf29ec8fb52ea
sancho56 <samimeskele@outlook.com>
2022-04-23 15:10:59 -0700

    string reverse using pointers

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,22 +8,19 @@ void rev_string(char *s)
 {
 	int i;
 	int a;
-	char *start, *end, ch; 
-	
-	end = s;
+	char *start, *end, ch;
 	start = s;
+	end = s;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		end++;
 	}
-
 	for (a = 0; a < i / 2; a++)
 	{
 		ch = *end;
 		*end = *start;
 		*start = ch;
-		
 		start++;
 		end--;
 	}
