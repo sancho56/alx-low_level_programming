d51b1af73c2eff77967e72fa59f7ab3063073885
sancho56 <samimeskele@outlook.com>
2022-05-06 01:26:12 +0300

    working with srings

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -12,12 +12,12 @@ char *_strncat(char *dest, char *src, int n)
 	int j;
 
 	for (i = 0; dest[i] != '\0'; i++);
-	for (j = 0; j < n; j++)
+	
+    for (j = 0; j < n; j++)
 	{
 		dest[i] = src[j];
 		i++;
 	}
-    for ( ; j < i; j++)
-        dest[j] = '\0';
+    dest[j + i] = '\0';
 	return (dest);
 }
