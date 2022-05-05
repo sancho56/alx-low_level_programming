e60b37413a7d156f9ba9e1ffbf36796d3391835c
sancho56 <samimeskele@outlook.com>
2022-05-05 23:58:14 +0300

    Concatenates strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,17 +9,16 @@ char *_strcat(char *dest, char *src)
 {
 	int i;
 	int j;
-	char *file;
-	file = dest;
+	char *file = dest;
 
-	for (i = 0; dest[i] != '\0'; i++)
-			file[i] = dest[i];
+	for (i = 0; dest[i] != '\0'; i++);
 	
 	for (j = 0; src[j] != '\0'; j++)
 	{
-			file[i] = src[j];
-			i++;
-	}	
+		file[i] = src[j];
+		i++;
+	}
+
 	return (file);
 }
 
