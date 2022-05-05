17917c91fbd8581139fa92538551721377145fc9
sancho56 <samimeskele@outlook.com>
2022-05-06 00:52:40 +0300

    working with strings

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,13 +9,13 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
-	int j;
 
-	for (i = 0; dest[i] != '\0'; i++);
-	for (j = 0; j < n; j++)
+	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
-		dest[i] = src[j];
+		dest[i] = src[i];
 		i++;
 	}
+    for ( ; i < n; i++)
+        dest[i] = '\0';
 	return (dest);
 }
