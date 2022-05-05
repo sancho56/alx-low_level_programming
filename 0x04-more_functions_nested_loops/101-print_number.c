68dcaf6d49e1f3633e4934ef7ba97e45f01e3d9b
sancho56 <samimeskele@outlook.com>
2022-05-05 19:17:57 +0300

    Prints an integer

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,39 +1,40 @@
 #include "main.h"
-/**
- * print_number - prints a integer
- * @n: integer
- * Return: void
- */
-void print_number(int n)
-{
-	if (n <= 0)
-	{
-		n *= -1;
-		if (n != 0)
-			putchar("-");
+#include <stdio.h>
 
-		if (n < 9)
-			putchar((n / 10) + '0');
-		putchar((n % 10) + '0');
-	}
-	else if (n < 9)
-		putchar(n);
-	else if (n < 99)
+/**                                                               
+ * print_number - prints a integer                                
+ * @n: integer                                                    
+ * Return: void                                                   
+ */                                                               
+void print_number(int n)                                          
+{
+        if (n <= 0)                                                
+        {
+                n *= -1;
+                if (n != 0)
+                        putchar('-');                                    
+                if (n > 9)                                        
+                        putchar((n / 10) + '0');                  
+                putchar((n % 10) + '0');                          
+        }                                                         
+        else if (n < 9)                                 
+                putchar(n);                                       
+        else if (n < 99)                                                   
+        {                                                         
+                putchar((n / 10) + '0');                          
+                putchar((n % 10) + '0');                          
+        }
+	else if ( n < 999)
 	{
-		putchar((n / 10) + '0');
-		putchar((n % 10) + '0');
-	}
-	else if (n < 999)
+                putchar((n / 100) + '0');
+                putchar(((n / 10) % 10) + '0');                          
+                putchar((n % 10) + '0');
+        }
+        else
 	{
-		putchar((n / 100) + '0');
-		putchar(((n / 10) % 10) + '0');
-		putchar((n % 10) + '0');
-	}
-	else
-	{
-		putchar((n / 1000) + '0');
-		putchar(((n / 100) % 10) + '0');
-		putchar(((n / 10) % 10) + '0');
-		putchar((n % 10) + '0');
-	}
+	        putchar((n / 1000) + '0');
+	        putchar(((n / 100) % 10)  + '0');
+                putchar(((n / 10) % 10) + '0');                          
+                putchar((n % 10) + '0');
+	  }
 }
