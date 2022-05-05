237c9f2b70e0445d98b4c6cdf592a05908f66675
sancho56 <samimeskele@outlook.com>
2022-05-05 02:08:15 -0700

    print integer

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,19 +6,34 @@
  */
 void print_number(int n)
 {
-	if (n > 0)
+	if (n <= 0)
 	{
 		n *= -1;
-		putchar("-");
-		if (n > 9)
+		if (n != 0)
+			putchar("-");
+
+		if (n < 9)
 			putchar((n / 10) + '0');
 		putchar((n % 10) + '0');
 	}
-	else if (n <= 0 && n > 9)
+	else if (n < 9)
 		putchar(n);
-	else
+	else if (n < 99)
 	{
 		putchar((n / 10) + '0');
+		putchar((n % 10) + '0');
+	}
+	else if (n < 999)
+	{
+		putchar((n / 100) + '0');
+		putchar(((n / 10) % 10) + '0');
+		putchar((n % 10) + '0');
+	}
+	else
+	{
+		putchar((n / 1000) + '0');
+		putchar(((n / 100) % 10) + '0');
+		putchar(((n / 10) % 10) + '0');
 		putchar((n % 10) + '0');
 	}
 }
