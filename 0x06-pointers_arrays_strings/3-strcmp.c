7a0cad551c99847b58a29c4ebae89d2355437ef8
sancho56 <samimeskele@outlook.com>
2022-05-06 02:53:49 +0300

    compares two strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strcmp - compare two strings
+ * @s1: char pointer
+ * @s2: char pointer
+ * Return: integer
+ */
+int _strcmp(char *s1, char *s2)
+{
+    while ((*s1 != '\0' && *s2 != '\0' ) && *s1 == *s2 )
+    {
+        s1++;
+        s2++;
+    }
+
+    if(*s1 == *s2)
+    {
+        return 0;
+    }
+    else
+    {
+        return *s1 - *s2;
+    }
+}
