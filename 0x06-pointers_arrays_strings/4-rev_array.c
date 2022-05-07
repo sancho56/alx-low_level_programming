4a5e7e473a68c81f05c662ba63d7b18a02ed0868
sancho56 <samimeskele@outlook.com>
2022-05-08 01:00:08 +0300

    re

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -6,13 +6,13 @@
  * Description: reverse array
  * Return: void
  */
-void reverse_array(int *a, int n)
+void reverse_array(int * a, int n)
 {
     int i;
 
     for (i = n - 1; i > 0; i--)
     {
-        _putchar(a[i]);
+        putchar(a[i]);
     }
-    _putchar('\n');
+    putchar('\n');
 }
