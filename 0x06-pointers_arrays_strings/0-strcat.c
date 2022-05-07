dd18563e8468ca972bc85ff58795374b83ee3c00
sancho56 <samimeskele@outlook.com>
2022-05-08 00:20:19 +0300

    concatenates strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _strcat - concatenates strings
+ * @dest: the destination string
+ * @src: the source string
+ * Return: char pointer
+ * 
+ */
+char *_strcat(char *dest, const char *src)
+{
+	
+}
