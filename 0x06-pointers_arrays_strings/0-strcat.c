e1d7fdeda1cf4327d39001402273eac96f2c27c7
sancho56 <samimeskele@outlook.com>
2022-05-08 00:34:38 +0300

    working with strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,18 +9,15 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int i;
-	int j;
+	int i, j;
 
 	for (i = 0; dest[i] != '\0'; i++)
 	{
-
 	}
-	for (j = 0; src[j] != '\0'; j++)
+	for (j = 0; src[j] != '\0'; j++, i++)
 	{
 		dest[i] = src[j];
-		i++;
 	}
 
-	return dest;
+	return (dest);
 }
