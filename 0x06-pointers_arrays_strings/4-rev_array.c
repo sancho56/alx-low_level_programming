8c22034843abe784d56dfae48ea6fb7fe9ed8a06
sancho56 <samimeskele@outlook.com>
2022-05-10 04:21:00 +0300

    working with arrays

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -39,11 +39,3 @@ void reverse_array(int *a, int n)
     }
     putchar('\n');
 }
-
-int main()
-{
-    int a[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 98, 1024, 1337};
-    
-    reverse_array(a, sizeof(a) / sizeof(int));
-    return (0);
-}
