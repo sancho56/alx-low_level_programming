c555cbd8f8eb9e3e99e1b3e76ff07b0b612671e5
sancho56 <samimeskele@outlook.com>
2022-05-10 05:05:58 +0300

    working with string

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -16,10 +16,10 @@ int _strcmp(char *s1, char *s2)
 
     if (*s1 == *s2)
     {
-        return 0;
+        return (0);
     }
     else
     {
-        return *s1 - *s2;
+        return (*s1 - *s2);
     }
 }
