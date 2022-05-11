908e1ef4294c0f9cb3c97fc4dafddeeec44d4b63
sancho56 <samimeskele@outlook.com>
2022-05-11 12:20:40 +0300

    working with strings

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -20,6 +20,6 @@ char *_strncat(char *dest, char *src, int n)
 		dest[i] = src[j];
 		i++;
 	}
-    dest[i + 1] = '\0';
+    dest[i] = '\0';
 	return (dest);
 }
--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
  * _strncpy - copies a string
- * @dest: char pointer
- * @src: char pointer
- * @n: integer number
+ * @dest : char pointer
+ * @src : char pointer
+ * @n : integer number
  * Description: the function to copy strings
  * Return: char pointer
  */
@@ -12,8 +12,11 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	for (i = 0; i < n && src[i] != '\0'; i++)
+	for (i = 0; (i < n && src[i] != '\0'); i++)
+    {
         dest[i] = src[i];
+    }
+        
     for ( ; i < n; i++)
         dest[i] = '\0';
 
