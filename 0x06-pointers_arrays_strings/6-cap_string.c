47c68d3cfdf55ef32cfacd57efab1ffd3cdb6bed
sancho56 <samimeskele@outlook.com>
2022-05-16 02:17:27 +0300

    capatalize the first letter of word

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * cap_string - capatalize the first letter of word 
+ * @ch: character pointer
+ * Return: character pointer
+ */
+char *cap_string(char *ch)
+{
+    int i;
+    int c;
+
+    for (i = 0; ch[i] != '\0'; i++)
+    {
+        if (ch[i] == " ")
+        {
+            c = 1;
+            continue;
+        }
+
+        if (c == 1)
+        {
+            if (ch[i] > 90)
+                ch[i] = ch[i] - 32;
+            c = 0;
+        }
+        ch[i] = ch[i];
+    }
+
+    return (ch);
+}
