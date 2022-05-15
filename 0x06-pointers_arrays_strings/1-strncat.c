0b5e99c336a949f5fbe77d48e09b90328f325631
sancho56 <samimeskele@outlook.com>
2022-05-16 00:54:20 +0300

    string Concatenates

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -11,11 +11,12 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-	int i, j;
+	int i;
+	int j;
 
 	for (i = 0; dest[i] != '\0'; i++);
 	
-    for (j = 0; j < n; j++)
+    for (j = 0; j <= n; j++)
 	{
 		dest[i] = src[j];
 		i++;
