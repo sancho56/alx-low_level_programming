b531f7d72f9d253a8d0ed6bfbebb37b39a7aff8e
sancho56 <samimeskele@outlook.com>
2022-05-16 10:11:52 +0300

    encodes a string

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -2,10 +2,25 @@
 /**
  * leet - encodes a string
  * @ch: character to be encoded
- * Description: This program encodes a string
  * Return: encoded pointer
  */
+
 char *leet(char *ch)
 {
+    char encode[] = "a4A4e3E3o0O0t7T7l1L1";
+    int i;
+    int j;
+
+    for (i = 0; ch[i] != '\0'; i++)
+    {
+        for (j = 0; encode[j] != '\0'; j++)
+        {
+            if (ch[i] == encode[j])
+            {
+                ch[i] = encode[j + 1];
+                break;
+            }
+        }
+    }
     return (ch);
 }
