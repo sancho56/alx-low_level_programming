5e4bf4212f441c4330a620fec39a7a834f4eb442
sancho56 <samimeskele@outlook.com>
2022-05-30 03:19:17 +0300

    appends source string

--- 0x06-pointers_arrays_strings/1-strcat.c
+++ 0x06-pointers_arrays_strings/1-strcat.c
@@ -6,13 +6,12 @@
  * Description: This will append string
  * Return: char pointer
  */
-char *_strcat (char *dest, char *src)
+char *_strcat(char *dest, char *src)
 {
 	int i;
 	int j;
 
 	for (i = 0; dest[i] != '\0'; i++);
-
 	for (j = 0; src[j] != '\0'; j++)
 	{
 		dest[i] = src[j];
